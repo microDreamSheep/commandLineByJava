614d57477661631da45d3fa22c44ea126cc49bac
microDreamSheep <atthemc313128460@gmail.com>
2022-11-30 12:18:54 +0800

    有限

--- src/main/java/live/midreamsheep/command/control/CoreController.java
+++ src/main/java/live/midreamsheep/command/control/CoreController.java
@@ -2,6 +2,8 @@ package live.midreamsheep.command.control;
 
 import live.midreamsheep.command.data.variable.GlobalVariable;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CoreController {
@@ -17,9 +19,47 @@ public class CoreController {
                 case "":
                     break;
                 default:
-                    String[] args = in.trim().split(" ");
+                    String[] args = parse(in);
                     CommandManager.getController(args[0]).control(args);
             }
         }
+    }
+    private String[] parse(String in){
+        List<String> list = new LinkedList<>();
+        char[] chars = in.toCharArray();
+        boolean StringFlag = false;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == '"'){
+                StringFlag = !StringFlag;
+                if(!StringFlag){
+                    i++;
+                    end = i;
+                    list.add(in.substring(in.indexOf("\"",start)+1,in.indexOf("\"",end-1)).trim());
+                    start = end;
+                }
+                continue;
+            }
+            if(StringFlag){
+                end++;
+                continue;
+            }
+            if(chars[i] != ' '){
+                end++;
+                continue;
+            }
+            if(chars[i] == ' '){
+                list.add(in.substring(start,end));
+                start = i+1;
+                end = i+1;
+            }
+
+        }
+        if(start != end){
+            list.add(in.substring(start,end));
+        }
+        System.out.println(list);
+        return list.toArray(new String[0]);
     }
 }
