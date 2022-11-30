7dad4e007664a83176a610bedfc095ceac82e943
microDreamSheep <atthemc313128460@gmail.com>
2022-11-30 11:30:34 +0800

    工具包实现

--- downloadExeTool/src/test/java/main.java
+++
@@ -1,9 +0,0 @@
-import live.midreamsheep.tools.download.DownloaderStarter;
-import org.junit.Test;
-
-public class main {
-    @Test
-    public void test(){
-        new DownloaderStarter().run("qq","1","E://临时文件");
-    }
-}
--- src/main/java/live/midreamsheep/command/control/CommandManager.java
+++ src/main/java/live/midreamsheep/command/control/CommandManager.java
@@ -1,8 +1,6 @@
 package live.midreamsheep.command.control;
 
 import live.midreamsheep.command.group.Controller;
-import live.midreamsheep.command.group.download.DownloadController;
-import live.midreamsheep.command.group.normal.command.Commmand;
 import live.midreamsheep.command.group.normal.file.CD;
 import live.midreamsheep.command.group.normal.file.DEL;
 import live.midreamsheep.command.group.normal.file.LS;
@@ -10,13 +8,11 @@ import live.midreamsheep.command.group.normal.file.PWD;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class CommandManager {
     private static final Map<String, Controller> controllerMap = new HashMap<>();
-    private static final Controller commmand = new Commmand();
     static{
-        //存入值
-        controllerMap.put("download", new DownloadController());
         //标准命令存入
         {
             //cd切换路径
@@ -30,6 +26,11 @@ public class CommandManager {
         }
     }
     public static Controller getController(String name){
-        return controllerMap.get(name)==null?commmand:controllerMap.get(name);
+        return Optional.ofNullable(controllerMap.get(name)).orElse(args -> {
+            System.err.println("命令不存在");
+        });
+    }
+    public static void addController(String name,Controller controller){
+        controllerMap.put(name,controller);
     }
 }
